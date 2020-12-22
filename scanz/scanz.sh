a85f8825dc4ac1be94ee7f38ce87ab1040e68bd1
Syd Salmon <syd@proganica.com>
2020-12-22 18:50:13 -0500

    [Δ] refined dirs & msgs.

--- scanz/scanz.sh
+++ scanz/scanz.sh
@@ -1,42 +1,42 @@
 #! /usr/local/bin/zsh
-cd /Users/syd/Downloads/0_rawScan/3_minimize/ || {
-    print 'Failure'
-    exit 1
+cd "$HOME"/Downloads/0_rawScan/3_minimize/ || {
+    print "The directory\n$HOME/Downloads/0_rawScan/3_minimize/\ndoes not exist."
 }
 min=$(find . -type f -maxdepth 1 | wc -l)
     if [ "$min" -eq 0 ]; then
-    exit
+    print "There is nothing in $PWD."
     else
-        mv -i /Users/syd/Downloads/0_rawScan/3_minimize/*.pdf /Users/syd/Downloads/0_rawScan/Ω-archive/
+        mv -i "$HOME"/Downloads/0_rawScan/3_minimize/*.pdf "$HOME"/Downloads/0_rawScan/Ω-archive/
     fi
-cd /Users/syd/Downloads/0_rawScan/2_optimize/ || {
-    print 'Failure'
-    exit 1
+
+cd "$HOME"/Downloads/0_rawScan/2_optimize/ || {
+    print "There is nothing in $PWD."
 }
 opt=$(find . -type f -maxdepth 1 | wc -l)
     if [ "$opt" -eq 0 ]; then
     exit
     else
-        mv -i /Users/syd/Downloads/0_rawScan/2_optimize/*.pdf /Users/syd/Downloads/0_rawScan/99_trash/
+        mv -i "$HOME"/Downloads/0_rawScan/2_optimize/*.pdf "$HOME"/Downloads/0_rawScan/99_trash/
     fi
-cd /Users/syd/Downloads/0_rawScan/1_binderize/*.pdf || {
-    print 'Failure'
-    exit 1
+
+cd "$HOME"/Downloads/0_rawScan/1_binderize || {
+    print "There is nothing in $PWD."
 }
 bnd=$(find . -type f -maxdepth 1 | wc -l)
     if [ "$bnd" -eq 0 ]; then
     exit
     else
-        mv -i /Users/syd/Downloads/0_rawScan/1_binderize/*.pdf /Users/syd/Downloads/0_rawScan/99_trash/
+        mv -i "$HOME"/Downloads/0_rawScan/1_binderize/*.pdf "$HOME"/Downloads/0_rawScan/99_trash/
     fi
-cd /Users/syd/Downloads/0_rawScan/*.pdf || {
-    print 'Failure'
-    exit 1
+
+cd "$HOME"/Downloads/0_rawScan/ || {
+    print "There is nothing in $PWD."
 }
 raw=$(find . -type f -maxdepth 1 | wc -l)
     if [ "$raw" -eq 0 ]; then
     exit
     else
-        mv -i /Users/syd/Downloads/0_rawScan/*.pdf /Users/syd/Downloads/0_rawScan/99_trash/
+        mv -i "$HOME"/Downloads/0_rawScan/*.pdf "$HOME"/Downloads/0_rawScan/99_trash/
     fi
 print 'Done!'
+cd "$HOME" || exit
