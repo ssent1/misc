63de2fcd7031e95a9df5529bf0099cd2881e8145
Syd Salmon <syd@proganica.com>
2020-12-22 19:03:06 -0500

    [Δ] generalized pathnames

--- scanz/populate-test_files.sh
+++ scanz/populate-test_files.sh
@@ -1,4 +1,4 @@
 #! /usr/local/bin/zsh
-for i in `seq 1 5`; touch /Users/syd/Downloads/0_rawScan/1_binderize/_binderize_test-0"$i".pdf &&
-for i in `seq 1 5`; touch /Users/syd/Downloads/0_rawScan/2_optimize/_optimize_test-0"$i".pdf &&
-for i in `seq 1 5`; touch /Users/syd/Downloads/0_rawScan/3_minimize/_minimize_test-0"$i".pdf
+for i in `seq 1 5`; touch $HOME/Downloads/0_rawScan/1_binderize/_binderize_test-0"$i".pdf &&
+for i in `seq 1 5`; touch $HOME/Downloads/0_rawScan/2_optimize/_optimize_test-0"$i".pdf &&
+for i in `seq 1 5`; touch $HOME/Downloads/0_rawScan/3_minimize/_minimize_test-0"$i".pdf
