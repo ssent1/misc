6578e892a14fa39c80545e4a75cf8ddca21a6f4a
Syd Salmon <syd@proganica.com>
2021-01-07 15:36:14 -0500

    [Δ] major change; better & faster.

--- scanz/scanz.sh
+++ scanz/scanz.sh
@@ -7,11 +7,13 @@ if [ "$min" -eq 0 ]; then
     print "There is nothing in $PWD."
 else
     for i in *.pdf; do
-        base=$(basename "$i".pdf)
+        base=$(basename "$i" .pdf)
         tar -czf "$HOME/Downloads/0_rawScan/77_archivze/$base-$(date +%Y%m%d_%H%M%S)".pdf.tar.gz "$i" &&
-            mv -i "$i" "$HOME/Downloads/0_rawScan/Ω-archive/$base".pdf
+            mv -n "$i" "$HOME/Downloads/0_rawScan/Ω-archive/$base"
     done
 fi
+# Creates Gzip archive with ISO date from minified PDFs.
+# Moves compressed archives to `77-archivze` and moves PDFs to `Ω-archive`.
 
 cd "$HOME"/Downloads/0_rawScan/2_optimize/ || {
     print "There is nothing in $PWD."
@@ -21,10 +23,11 @@ if [ "$opt" -eq 0 ]; then
     exit
 else
     for j in *.pdf; do
-        base=$(basename "$j".pdf)
-        mv -i "$j" "$HOME/Downloads/0_rawScan/2_optimize/$base-$(date +%Y%m%d_%H%M%S)".pdf
+        base=$(basename "$j" .pdf)
+        mv -n "$j" "$HOME/Downloads/0_rawScan/99_trash/$base-$(date +%Y%m%d_%H%M%S)".pdf
     done
 fi
+# Clears out PDFs in `2_optimize` by adding ISO date and moving the files to `99_trash`.
 
 cd "$HOME"/Downloads/0_rawScan/1_binderize || {
     print "There is nothing in $PWD."
@@ -34,10 +37,15 @@ if [ "$bnd" -eq 0 ]; then
     exit
 else
     for k in *.pdf; do
-        base=$(basename "$k".pdf)
-        mv -i "$k" "$HOME/Downloads/0_rawScan/1_binderize/$base-$(date +%Y%m%d_%H%M%S)".pdf
+        base=$(basename "$k" .pdf)
+        mv -n "$k" "$HOME/Downloads/0_rawScan/99_trash/$base-$(date +%Y%m%d_%H%M%S)".pdf
+    done
+    for k in *.ai; do
+        base=$(basename "$k" .ai)
+        mv -n "$k" "$HOME/Downloads/0_rawScan/99_trash/$base-$(date +%Y%m%d_%H%M%S)".ai
     done
 fi
+# Clears out AIs and PDFs in `1_binderize` by adding ISO date and moving the files to `99_trash`.
 
 cd "$HOME"/Downloads/0_rawScan/ || {
     print "There is nothing in $PWD."
@@ -46,14 +54,14 @@ raw=$(find . -type f -maxdepth 1 | wc -l)
 if [ "$raw" -eq 0 ]; then
     exit
 else
-    for l in *.pdf; do
-        base=$(basename "$l".pdf)
-        mv -i "$l" "$HOME/Downloads/0_rawScan/99_trash/$base-$(date +%Y%m%d_%H%M%S)".pdf
+    for h in *.pdf; do
+        base=$(basename "$h" .pdf)
+        mv -n "$h" "$HOME/Downloads/0_rawScan/99_trash/$base-$(date +%Y%m%d_%H%M%S)".pdf
     done
 fi
+
 print 'Done!'
 cd "$HOME"/Code/misc/linkz/ || {
     print "The directory\n$HOME/Code/misc/linkz/\ndoes not exist."
 }
 ./linkz.sh
-
