653f14e493af9ca97b030d026fcee7f1dd09e8f1
Syd Salmon <syd@proganica.com>
2021-01-07 00:28:20 -0500

    [Δ] added dir to pop…-test_files

--- scanz/populate-test_files.sh
+++ scanz/populate-test_files.sh
@@ -1,4 +1,5 @@
 #! /usr/local/bin/zsh
+for i in `seq 1 5`; touch $HOME/Downloads/0_rawScan/_rawScan_test-0"$i".pdf &&
 for i in `seq 1 5`; touch $HOME/Downloads/0_rawScan/1_binderize/_binderize_test-0"$i".pdf &&
 for i in `seq 1 5`; touch $HOME/Downloads/0_rawScan/2_optimize/_optimize_test-0"$i".pdf &&
 for i in `seq 1 5`; touch $HOME/Downloads/0_rawScan/3_minimize/_minimize_test-0"$i".pdf
