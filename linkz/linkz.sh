275bc5b9cf665d3f6adad46bce00efcc514ff06a
Syd Salmon <syd@proganica.com>
2021-01-07 15:54:07 -0500

    [Δ] Include AI & PDF. Added for-loop. Faster.

--- linkz/linkz.sh
+++ linkz/linkz.sh
@@ -2,11 +2,20 @@
 cd "$HOME"/Downloads/0_rawScan/0_templatize/ || {
     print "The directory\n$HOME/Downloads/0_rawScan/0_templatize/\ndoes not exist."
 }
-min=$(find . -type f -name '*.pdf' -maxdepth 1 | wc -l)
-    if [ "$min" -eq 0 ]; then
-    print "There are no PDFs in $PWD."
-    else
-        ln -f "$HOME"/Downloads/0_rawScan/0_templatize/*.pdf "$HOME"/Downloads/0_rawScan/1_binderize/
-    fi
+min=$(find . -type f -maxdepth 1 -name '*.pdf' -or -name '*.ai' | wc -l)
+if [ "$min" -eq 0 ]; then
+    print "There are no AI or PDF filess in $PWD."
+else
+
+    for i in *.ai; do
+        ln -f "$i" "$HOME"/Downloads/0_rawScan/1_binderize/
+    done
+
+    for i in *.pdf; do
+        ln -f "$i" "$HOME"/Downloads/0_rawScan/1_binderize/
+    done
+
+fi
 printf "%s\n \n###########################################################\n$min hard links have been created in 1_binderize.\n###########################################################\n"
 cd "$HOME" || return
+
