809fa3c1aafe078f713f0510d3e11af1beebd46b
Syd Salmon <syd@proganica.com>
2021-06-30 23:26:30 -0400

    [~]increaded portability; add error handling

--- Mac_Daddy/mac_daddy.zsh
+++ Mac_Daddy/mac_daddy.zsh
@@ -1,31 +1,31 @@
 (
     IFS=$'\n'
-    array=("$(grep -i {query} mac_daddy)")
+    array=("$(grep -i {query} DBmacDaddy)")
 
-    echo '<?xml version="1.0"?>'
-    echo '<items>'
-    echo ''
+    printf "%s\n" '<?xml version="1.0"?>'
+    printf "%s\n" '<items>'
+    printf "%s\n" ''
 
     for i in "${array[@]}"; do
-        macaddr=("$(echo "$i" | cut -d\, -f1)")
-        device=("$(echo "$i" | cut -d\, -f4)")
-        ipaddr=("$(echo "$i" | cut -d\, -f3)")
+        macaddr=("$(printf "%s\n" "$i" | cut -d\, -f1)")
+        device=("$(printf "%s\n" "$i" | cut -d\, -f4)")
+        ipaddr=("$(printf "%s\n" "$i" | cut -d\, -f3)")
 
         # concatenate variables
         output="$macaddr --> ($device)"
 
         # return value(s) to {query} in Alfred
-        echo "<item arg=\"$output\" valid=\"yes\">"
+        printf "%s\n" "<item arg=\"$output\" valid=\"yes\">"
 
         # display output in Alfred's GUI
-        echo "<title>$device ↔︎ $ipaddr ↔︎ $macaddr</title>"
-        echo "<subtitle>☛ Run 'sudo nmap -sP 192.168.1.0/24' if IP is outdated.</subtitle>"
-        echo "<icon>mac_daddy.png</icon></item>"
-        echo ""
+        printf "%s\n" "<title>$device ↔︎ $ipaddr ↔︎ $macaddr</title>"
+        printf "%s\n" "<subtitle>|-> Run 'sudo nmap -sP 192.168.1.0/24' if IP is outdated.</subtitle>"
+        printf "%s\n" "<icon>mac_daddy.png</icon></item>"
+        printf "%s\n" ""
 
     done
 
-    echo "</items>"
+    printf "%s\n" "</items>"
 )
 
 # script: sudo nmap -sP 192.168.1.0/24
