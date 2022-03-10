9842aba66001ff2d02d358e2b636dab35d146137
Syd Salmon <syd@proganica.com>
2022-03-10 00:29:45 -0500

    brewster: + JS (npm) & Python3 (pip)

--- Brewster/brewster.zsh
+++ Brewster/brewster.zsh
@@ -1,5 +1,18 @@
-#!/bin/zsh
+#d/bin/zsh
+# update Homebrew
 brew upgrade
 brew update
 brew cleanup
 brew doctor
+
+# update npm - javascript package manager
+npm update
+npm prune
+
+# python3 pip
+# python3 -m pip
+# pip 22.0.4 from /usr/local/lib/python3.9/site-packages/pip (python 3.9)
+# build update list
+python3 -m pip list | awk '(NR <=2) {next} /^([a-zA-Z0-9]+).*/ {print "python -m pip install -U " $1}' >pip-squeak
+sleep 2
+./pip-squeak
