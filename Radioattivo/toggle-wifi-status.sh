5a772b40a67382bbfd41cdc48209dbce7a0c8ac0
ssent1 <ssent1@gmail.com>
2022-09-17 17:22:31 -0400

    radioattivo:

---
+++ Radioattivo/toggle-wifi-status.sh
@@ -0,0 +1,10 @@
+on alfred_script(q)
+set wifi to (do shell script "networksetup -listallhardwareports | grep -A 1 Wi-Fi | tail -n 1 | cut -b 9-12")
+if (offset of "On" in (do shell script "networksetup -getairportpower " & wifi & " ")) > 0 then
+do shell script "networksetup -setairportpower " & wifi & " off"
+    set output to (" off")
+else
+do shell script "networksetup -setairportpower " & wifi & " on"
+    set output to (" on")
+end if
+end alfred_script
