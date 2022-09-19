30af806eea8f26acb9c87c42a8ed5c3cc26aa3b7
ssent1 <ssent1@gmail.com>
2022-09-19 18:07:44 -0400

    Radioattivo:

--- Radioattivo/get-wifi-status.sh
+++ Radioattivo/get-wifi-status.sh
@@ -1,17 +1,13 @@
 #! /bin/zsh
 wifi=$(networksetup -getairportpower en1)
-
 cat << EOF
 <?xml version="1.0"?>
 <items>
-
-<item uid="com.sydsalmon.radioattivo" arg="$wifi" valid="yes" type="file">
-<title>Wi-Fi Status</title>
-<subtitle>$wifi</subtitle>
-<icon type="file">wifi-logo.png</icon>
-</item>
-
+    <item uid="com.sydsalmon.radioattivo" arg="$wifi" valid="yes" type="file">
+    <title>Wi-Fi Status</title>
+    <subtitle>$wifi</subtitle>
+    <icon type="file">wifi-logo.png</icon>
+    </item>
 </items>
-
 EOF
 # https://apple.stackexchange.com/questions/171160/how-to-get-the-wi-fi-connection-status-from-terminal
