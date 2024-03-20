8ea370beb9e7515a04fc69beda22d37ba8a2c11b
Syd Salmon <syd@proganica.com>
2024-03-20 02:52:41 -0400

    Initial

---
+++ scripts/lma.zsh
@@ -0,0 +1,10 @@
+#!/bin/zsh
+# Launch Micro Productivity Apps
+
+open -gj /Applications/Breeze.app /Applications/Itsycal.app /Applications/Numi.app
+
+# -g  Do not bring the application to the foreground.
+# -j  Launches the app hidden.
+
+# ^ 2024-03-19T17:41:13-0400
+# % 2024-03-19T19:49:22-0400
