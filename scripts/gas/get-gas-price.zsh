776052a70c44cf78e95bb745449d68a7973f5f28
Syd Salmon <syd@proganica.com>
2024-07-04 16:56:36 -0400

    Modularized code, added functions.

--- scripts/gas/get-gas-price.zsh
+++ scripts/gas/get-gas-price.zsh
@@ -1,5 +1,6 @@
 #!/bin/zsh
 
+get_gas_price() {
 # URL of the gas station
 url="https://www.gasbuddy.com/station/198389"
 
@@ -22,14 +23,19 @@ rtime=$(echo "$content" |
 
 # Convert price to a floating-point number
 price_float=$(echo $price | awk '{print $1/100}')
+}
 
+print_gas_price() {
 # Print the result
 GREEN='\033[1;32m'
 NC='\033[0m'
+
+get_gas_price
 echo "Regular gas: ${GREEN}\$$price_float${NC}\n$stn, ~$rtime"
+}
 
 # sources
 # https://claude.ai/chat/88b08358-7146-495d-b013-24e0c7967d59 "Zsh Script to Scrape Local Gas Prices"
 
 # ^ 2024-06-28T13:53:54-0400\
-# % 2024-07-04T16:07:20-0400
+# % 2024-07-04T16:40:23-0400
