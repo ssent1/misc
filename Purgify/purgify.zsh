44115399741d015643fe58f4a56e6ba78905bec9
Syd Salmon <syd@proganica.com>
2024-07-04 23:11:46 -0400

    Updated

--- Purgify/purgify.zsh
+++ Purgify/purgify.zsh
@@ -1,8 +1,8 @@
 #!/bin/zsh
-find / -name ".DS_Store" -exec rm -Rf {} \; 2> /dev/null
-find / -name ".apdisk" -exec rm -Rf {} \;  2> /dev/null
-find / -name ".AppleDouble" -exec rm -Rf {} \; 2> /dev/null
-find / -name ".AppleDB" -exec rm -Rf {} \; 2> /dev/null
-find / -name "afpd.core" -exec rm -Rf {} \; 2> /dev/null
-find / -name ".TemporaryItems" -exec rm -Rf {} \; 2> /dev/null
-find / -name "__MACOSX" -exec rm -Rf {} \; 2> /dev/null
+find ~ -name ".DS_Store" -exec rm -Rf {} \; 2> /dev/null
+find ~ -name ".apdisk" -exec rm -Rf {} \;  2> /dev/null
+find ~ -name ".AppleDouble" -exec rm -Rf {} \; 2> /dev/null
+find ~ -name ".AppleDB" -exec rm -Rf {} \; 2> /dev/null
+find ~ -name "afpd.core" -exec rm -Rf {} \; 2> /dev/null
+find ~ -name ".TemporaryItems" -exec rm -Rf {} \; 2> /dev/null
+find ~ -name "__MACOSX" -exec rm -Rf {} \; 2> /dev/null
